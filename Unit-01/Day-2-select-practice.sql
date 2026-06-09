87df12b9011469fa99174defa3d7da77fe7cebcc
abdullah58091 <abdullahjd6@gmail.com>
2026-06-09 20:29:31 +0530

    Add Day 2 SELECT and WHERE SQL practice

---
+++ Unit-01/Day-2-select-practice.sql
@@ -0,0 +1,19 @@
+USE college;
+
+SELECT * FROM students;
+
+SELECT name FROM students;
+
+SELECT * FROM students
+WHERE id = 2;
+
+SELECT * FROM students
+WHERE age >= 21;
+
+SELECT * FROM students
+ORDER BY age;
+
+SELECT * FROM students
+ORDER BY age DESC;
+
+
