4b642a9bc4f9bd6157a48924ec98be0735076230
abdullah58091 <abdullahjd6@gmail.com>
2026-07-06 16:39:05 +0530

    problem practice

---
+++ Practice-SQL.sql
@@ -0,0 +1,148 @@
+
+mysql> SHOW DATABASE;
+ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'DATABASE' at line 1
+mysql> SHOW DATABASES ;
++--------------------+
+| Database           |
++--------------------+
+| college            |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+5 rows in set (0.03 sec)
+
+mysql> USE COLLEGE ;
+Database changed
+mysql> SHOW TABLES ;
++-------------------+
+| Tables_in_college |
++-------------------+
+| marks             |
+| students          |
++-------------------+
+2 rows in set (0.01 sec)
+
+mysql> SECLECT * FROM students ;
+ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'SECLECT * FROM students' at line 1
+mysql> SELECT * FROM students ;
++----+----------+------+
+| id | name     | age  |
++----+----------+------+
+|  1 | Abdullah |   21 |
+|  2 | Iram     |   18 |
+|  3 | Arsh     |   25 |
+|  4 | Ali      |   21 |
+|  5 | Sara     |   19 |
+|  6 | Zoya     |   22 |
+|  7 | pooja    |   34 |
+|  8 | saif     |   45 |
+|  9 | Jhon     |   33 |
+| 10 | Ajaye    |   56 |
+| 11 | Sadik    |   23 |
++----+----------+------+
+11 rows in set (0.02 sec)
+
+mysql> SELECT * FROM students
+    -> WHERE students_id = 5;
+ERROR 1054 (42S22): Unknown column 'students_id' in 'where clause'
+mysql> SELECT * FROM students
+    -> WHERE student_id = 5;
+ERROR 1054 (42S22): Unknown column 'student_id' in 'where clause'
+mysql> SELECT * FROM students WHERE student_id = 5;
+ERROR 1054 (42S22): Unknown column 'student_id' in 'where clause'
+mysql> SELECT * FROM students
+    -> WHERE id = 5;
++----+------+------+
+| id | name | age  |
++----+------+------+
+|  5 | Sara |   19 |
++----+------+------+
+1 row in set (0.00 sec)
+
+mysql> EXPLAIN
+    -> SELECT * FROM students
+    -> WHERE age > 18;
++----+-------------+----------+------------+------+---------------+------+---------+------+------+----------+-------------+
+| id | select_type | table    | partitions | type | possible_keys | key  | key_len | ref  | rows | filtered | Extra       |
++----+-------------+----------+------------+------+---------------+------+---------+------+------+----------+-------------+
+|  1 | SIMPLE      | students | NULL       | ALL  | NULL          | NULL | NULL    | NULL |    9 |    33.33 | Using where |
++----+-------------+----------+------------+------+---------------+------+---------+------+------+----------+-------------+
+1 row in set, 1 warning (0.00 sec)
+
+mysql> EXPLAIN
+    -> SELECT * FROM students
+    -> WHERE id = 5;
++----+-------------+----------+------------+-------+---------------+---------+---------+-------+------+----------+-------+
+| id | select_type | table    | partitions | type  | possible_keys | key     | key_len | ref   | rows | filtered | Extra |
++----+-------------+----------+------------+-------+---------------+---------+---------+-------+------+----------+-------+
+|  1 | SIMPLE      | students | NULL       | const | PRIMARY       | PRIMARY | 4       | const |    1 |   100.00 | NULL  |
++----+-------------+----------+------------+-------+---------------+---------+---------+-------+------+----------+-------+
+1 row in set, 1 warning (0.00 sec)
+
+mysql> CREATE INDEX idx_age ON students(age);
+Query OK, 0 rows affected (0.06 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> EXPLAIN
+    -> SELECT * FROM students
+    -> WHERE age > 18;
++----+-------------+----------+------------+------+---------------+------+---------+------+------+----------+-------------+
+| id | select_type | table    | partitions | type | possible_keys | key  | key_len | ref  | rows | filtered | Extra       |
++----+-------------+----------+------------+------+---------------+------+---------+------+------+----------+-------------+
+|  1 | SIMPLE      | students | NULL       | ALL  | idx_age       | NULL | NULL    | NULL |    9 |   100.00 | Using where |
++----+-------------+----------+------------+------+---------------+------+---------+------+------+----------+-------------+
+1 row in set, 1 warning (0.00 sec)
+
+mysql> EXPLAIN
+    -> SELECT * FROM students
+    -> WHERE age = 56;
++----+-------------+----------+------------+------+---------------+---------+---------+-------+------+----------+-------+
+| id | select_type | table    | partitions | type | possible_keys | key     | key_len | ref   | rows | filtered | Extra |
++----+-------------+----------+------------+------+---------------+---------+---------+-------+------+----------+-------+
+|  1 | SIMPLE      | students | NULL       | ref  | idx_age       | idx_age | 5       | const |    1 |   100.00 | NULL  |
++----+-------------+----------+------------+------+---------------+---------+---------+-------+------+----------+-------+
+1 row in set, 1 warning (0.00 sec)
+
+mysql> EXPLAIN
+    -> SELECT * FROM students
+    -> WHERE age = 21;
++----+-------------+----------+------------+------+---------------+---------+---------+-------+------+----------+-------+
+| id | select_type | table    | partitions | type | possible_keys | key     | key_len | ref   | rows | filtered | Extra |
++----+-------------+----------+------------+------+---------------+---------+---------+-------+------+----------+-------+
+|  1 | SIMPLE      | students | NULL       | ref  | idx_age       | idx_age | 5       | const |    2 |   100.00 | NULL  |
++----+-------------+----------+------------+------+---------------+---------+---------+-------+------+----------+-------+
+1 row in set, 1 warning (0.00 sec)
+
+mysql> EXPLAIN
+    -> SELECT * FROM students
+    -> WHERE id = 10;
++----+-------------+----------+------------+-------+---------------+---------+---------+-------+------+----------+-------+
+| id | select_type | table    | partitions | type  | possible_keys | key     | key_len | ref   | rows | filtered | Extra |
++----+-------------+----------+------------+-------+---------------+---------+---------+-------+------+----------+-------+
+|  1 | SIMPLE      | students | NULL       | const | PRIMARY       | PRIMARY | 4       | const |    1 |   100.00 | NULL  |
++----+-------------+----------+------------+-------+---------------+---------+---------+-------+------+----------+-------+
+1 row in set, 1 warning (0.00 sec)
+
+mysql> EXPLAIN
+    -> SELECT * FROM students
+    -> WHERE age BETWEEN 20 AND 30;
++----+-------------+----------+------------+-------+---------------+---------+---------+------+------+----------+-----------------------+
+| id | select_type | table    | partitions | type  | possible_keys | key     | key_len | ref  | rows | filtered | Extra                 |
++----+-------------+----------+------------+-------+---------------+---------+---------+------+------+----------+-----------------------+
+|  1 | SIMPLE      | students | NULL       | range | idx_age       | idx_age | 5       | NULL |    5 |   100.00 | Using index condition |
++----+-------------+----------+------------+-------+---------------+---------+---------+------+------+----------+-----------------------+
+1 row in set, 1 warning (0.04 sec)
+
+mysql> EXPLAIN
+    -> SELECT * FROM students
+    -> WHERE age < 20;
++----+-------------+----------+------------+-------+---------------+---------+---------+------+------+----------+-----------------------+
+| id | select_type | table    | partitions | type  | possible_keys | key     | key_len | ref  | rows | filtered | Extra                 |
++----+-------------+----------+------------+-------+---------------+---------+---------+------+------+----------+-----------------------+
+|  1 | SIMPLE      | students | NULL       | range | idx_age       | idx_age | 5       | NULL |    2 |   100.00 | Using index condition |
++----+-------------+----------+------------+-------+---------------+---------+---------+------+------+----------+-----------------------+
+1 row in set, 1 warning (0.00 sec)
+
+mysql>
