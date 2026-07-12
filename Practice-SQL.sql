248319ee2829b2e2eec69d8372c469b5239e3caa
abdullah58091 <abdullahjd6@gmail.com>
2026-07-12 22:01:33 +0530

    problem solve

--- Practice-SQL.sql
+++ Practice-SQL.sql
@@ -300,3 +300,26 @@ mysql> SELECT * FROM students ;
 11 rows in set (0.00 sec)
 
 mysql>
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] temp = Arrays.copyOf(arr, arr.length);
+
+        Arrays.sort(temp);
+
+        int rank = 1;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int ele : temp) {
+            if (!hm.containsKey(ele)) {
+                hm.put(ele, rank++);
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = hm.get(arr[i]);
+        }
+
+        return arr;
+    }
+}
